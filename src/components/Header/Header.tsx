a109441cb9a2807e104b73f54910030ed6b41aa2
fbnjeje <fabiangomes2015@gmail.com>
2025-01-05 18:12:04 -0500

    fixing header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import './styles.css';
+
+import Icon from '../../assets/platzi.svg';
+import arrowDown from '../../assets/arrow-down.svg';
 const Header = () => {
   return (
     <div className='header'>
 
       {/* logo */}
       <view >
-        <p>Logo</p>
+
+        <img src={Icon} width="91" height="31" className='pointer'></img>
       </view>
 
       {/* nav */}
       <view>
         <nav>
-          <ul className='nav'>
-            <li>Explorar</li>
-            <li>Comunidad</li>
-            <li>Planes</li>
-            <li>Empresas</li>
+
+          <ul className='header-nav'>
+            <li className='header-explorar pointer'>Explorar</li>
+            <li className='header-explorar pointer'>Comunidad <img src={arrowDown} width="15" height="15"></img></li>
+            <li className='pointer'>Planes</li>
+            <li className='pointer'>Empresas</li>
           </ul>
         </nav>
       </view>
 
       {/* button */}
       <view>
-        <button>Acceder</button>
+        <button className='header-button pointer'>Acceder</button>
       </view>
     </div>
   );
