e238bd1d127d979e5e5fef45fae65d138fee3390
fbnjeje <fabiangomes2015@gmail.com>
2025-01-06 11:54:10 -0500

    add home section

--- src/Sections/Home/Home.tsx
+++ src/Sections/Home/Home.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './styles.css'
+
+import Search from '../../assets/search.svg'
 function Home() {
     return (
-        <div className='heroSection'>
-            Caiganlen
-        </div>
+        <section className='heroSection'>
+            <h1 className='title'>La Escuela de Tecnología <br /><span>de Latinoameríca</span></h1>
+            <p className='description'>
+                Más de 5 millones de estudiantes y más de 3.000 empresas aprenden en Platzi
+            </p>
+            <article className='coursesSearch'>
+                <label className='searchLabel'>¿Qué quieres aprender?</label>
+                <input type="text" placeholder='holi' maxLength={512} className='inputAutocomplete' />
+                <button className='searchIconBtn'>
+                    <img src={Search} alt="" />
+                </button>
+            </article>
+            <h6 className='freeCourses'>
+                Empieza cualquier curso sin costo.
+                <br />
+                O consigue
+                <a href=""> Platzi para tu empresa</a>
+            </h6>
+        </section>
     )
 }
 
