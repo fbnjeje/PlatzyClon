d6abfa13e049d9b978e071977bba0dbeff51f9e1
fbnjeje <fabiangomes2015@gmail.com>
2025-01-14 11:35:36 -0500

    add generator section

--- src/Sections/Generator/GeneratorSection.tsx
+++ src/Sections/Generator/GeneratorSection.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './styles.css'
+
 function GeneratorSection() {
     return (
         <section className='generator-section'>
             <div className='container'>
-                <h2 className='title'>Descubre tu ruta de aprendizaje personalizada</h2>
-                <form action="">
-                    <textarea className="LpGenerator_LpGeneratorTextarea__us1tE" placeholder="Quiero crear experiencias de usuario atractivas en la web, escribir y crear contenido"></textarea>
+                <h2 className='generator-title'>Descubre tu ruta de aprendizaje personalizada</h2>
+                <form action="" className='generator-form'>
+                    <textarea className="generator-textarea" placeholder="Quiero crear experiencias de usuario atractivas en la web, escribir y crear contenido"></textarea>
                     <span style={{ display: 'none' }}></span>
-                    <button>Generar ruta</button>
+                    <button className='generator-button'>
+                        <span>Generar mi ruta</span>
+                    </button>
                 </form>
             </div>
         </section>
